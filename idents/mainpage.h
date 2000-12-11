77ea07e589f2a09ee34c56e12231b9571595e0cf
burnett <>
2000-12-11 16:36:17 +0000

    New package to hold identifiers only. No dependence on other packages or environments is anticipated

---
+++ idents/mainpage.h
@@ -0,0 +1,19 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/reconstruction/reconstruction/mainpage.h,v 1.1 2000/11/27 02:24:57 burnett Exp $
+// Mainpage for doxygen
+
+/*! \mainpage package idents
+
+This package contains identifier definitions for 
+- towers (ModuleId)
+- crystals (XtalId)
+- ACD Tiles (ScintillatorId) 
+- Tracker strips
+
+<hr>
+  \section notes release notes
+  \include release.notes
+  \section requirements requirements
+  \include requirements
+
+*/
+
