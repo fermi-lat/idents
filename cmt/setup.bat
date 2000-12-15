a435cee7317d41d81f03c6fca8e39ed831bf2af4
burnett <>
2000-12-15 01:38:43 +0000

    Add comments, access methods to XtalId.

--- cmt/setup.bat
+++ cmt/setup.bat
@@ -1,11 +1,11 @@
 echo off
 IF .%1==. (set CMTCONFIG=VisualC) ELSE set CMTCONFIG=%1
 set CMTROOT=c:\packages\CMT\v1r5p1
-set GLAST_SETTINGSROOT=c:\packages\external\glast_settings\v2
+set GLAST_SETTINGSROOT=c:\packages\external\glast_settings\v3
 set GLAST_SETTINGSCONFIG=%CMTCONFIG%
 set EXTLIBROOT=c:\packages\external\EXTLIB\v2r2
 set EXTLIBCONFIG=%CMTCONFIG%
-set IDENTSROOT=c:\packages\gaudi_dev\idents\v1
+set IDENTSROOT=c:\packages\gaudi_dev\idents\v1r1
 set IDENTSCONFIG=%CMTCONFIG%
 set EXT_DIR=C:\extlib
 set LHCXX_DIR=%EXT_DIR%
