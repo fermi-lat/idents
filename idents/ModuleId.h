03a6cdeda705c342de1d7eb09eb783d1f261242a
burnett <>
2001-03-20 20:52:42 +0000

    restore ModuleId to AO convention

--- idents/ModuleId.h
+++ idents/ModuleId.h
@@ -1,9 +1,11 @@
-// $Id: ModuleId.h,v 1.1.1.1 2000/12/11 16:36:17 burnett Exp $
+// $Id: ModuleId.h,v 1.1 2000/12/11 16:36:17 burnett Exp $
 
 #ifndef GLAST_MODULEID_H
 #define GLAST_MODULEID_H 1
 
 /** define the tower id used in reconstruction
+    Note this does not conform x and y index conventions.
+    Please use TowerId if that is important.
 
   */
 namespace idents {
@@ -12,23 +14,19 @@ class ModuleId
 {
   public:
 
-      /*! Number of Y towers is used for the tower arithmetic
-          Luckily, this works for the BTEM/BFEM, because the tower
-          id is always zero anyway.
-      */
-      enum {yNum=4}; // not likely to change again
+      enum {yNum=4, yNum=4}; // not likely to change again
 
       //! create from another Id (0..15)
       ModuleId (unsigned int id = 0):m_id(id){}
 
-      //! create from x, y indices (each 0..3)
-      ModuleId (unsigned int ix, unsigned int iy):m_id(ix+yNum*iy){}
+      //! create from x, y indeces (each 1..4)
+      ModuleId (unsigned int ix, unsigned int iy):m_id(ix-1+xNum*(iy-1)){}
 
-      //! access to the x index (0..3)
-      int ix () const {return (m_id % yNum);}
+      //! access to the x index (1..4)
+      int ix () const {return (m_id % xNum)+1;}
 
-      //! access to the y index (0..3)
-      int iy () const {return (m_id / yNum);}
+      //! access to the y index (1..4)
+      int iy () const {return (m_id / xNum)+1;}
 
       //! is this module a neighbor?
       bool neighbor (const ModuleId& n){
