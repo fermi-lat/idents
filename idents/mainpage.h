4b292a8a52ed887a9651c22a202112d890c47cf9
burnett <>
2002-03-08 23:05:56 +0000

    a little formatting, slight improvements.

--- idents/mainpage.h
+++ idents/mainpage.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/reconstruction/reconstruction/mainpage.h,v 1.1 2000/11/27 02:24:57 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/idents/idents/mainpage.h,v 1.1.1.1 2000/12/11 16:36:17 burnett Exp $
 // Mainpage for doxygen
 
 /*! \mainpage package idents
@@ -8,6 +8,7 @@ This package contains identifier definitions for
 - crystals (XtalId)
 - ACD Tiles (ScintillatorId) 
 - Tracker strips
+- Volumes (VolumeIdentifier)
 
 <hr>
   \section notes release notes
