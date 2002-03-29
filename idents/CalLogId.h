f68e0e82472445ede1887bd2b7c0408d452bb4dd
richard <>
2002-03-29 00:52:41 +0000

    restore >>, << overrides; add comments from Eric

--- idents/CalLogId.h
+++ idents/CalLogId.h
@@ -21,6 +21,16 @@ using namespace std;
 //		Serialize methods give i/o for packed ID.
 //		Extractor/inserter give i/o for unpacked ID.
 //		Input methods ensure that packed and unpacked IDs stay in sync.
+//
+//    Retrieve packed ID or unpacked tower, layer, and column 
+//        inline int getPackedId() const 
+//        void getUnpackedId(short& tower, short& layer, short& column); 
+// 
+//    Retrieve tower, layer, and column numbers individually from packed ID 
+//        inline short getTower() const 
+//        inline short getLayer() const 
+//        inline short getColumn() const 
+
 //              
 // Author:  J. Eric Grove	22 Mar 2001                      
 //------------------------------------------------------------------------------
@@ -74,9 +84,29 @@ public:
 
         operator int() const {return m_packedId;};
 
+       	friend ostream& operator<< (ostream &stream, CalLogId logId);
+    	friend istream& operator>> (istream &stream, CalLogId &logId);
+
 };
 
 
+// overload the inserter to stream unpacked tower, layer and column
+ostream& operator<<(ostream &stream, CalLogId logId)
+{
+        stream << logId.m_tower << " ";
+	stream << logId.m_layer << " ";
+	stream << logId.m_column << " ";
+	return stream;
+}
+
+
+// extract unpacked ID, and stuff packed ID from unpacked info
+istream& operator>> (istream &stream, CalLogId &logId)
+{
+	stream >> logId.m_tower >> logId.m_layer >> logId.m_column;
+	logId.packId(logId.m_tower, logId.m_layer, logId.m_column);		// ensure m_packedId is synced
+	return stream;
+}
 
 
 /// retrieve unpacked ID, tower, layer, and column
