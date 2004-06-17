4a58840b3898bfa426daafe913f2255b543f94e9
jrb <>
2004-06-17 21:39:13 +0000

    Enlarge to test all the new stuff

--- src/test/test_idents.cxx
+++ src/test/test_idents.cxx
@@ -1,9 +1,10 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/idents/src/test/test_idents.cxx,v 1.3 2002/12/30 20:36:04 heather Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/idents/src/test/test_idents.cxx,v 1.4 2004/06/14 21:18:33 jrb Exp $
 
 
 #include "idents/VolumeIdentifier.h"
 #include "idents/AcdId.h"
 #include "idents/CalXtalId.h"
+#include "idents/TkrId.h"
 #include <map>
 #include <vector>
 #include <iostream>
@@ -56,6 +57,9 @@ int main()
   std::cout << acdId.tile() << " " << acdId.ribbon() << " " << acdId.ribbonNum() 
       << " " << acdId.ribbonOrientation() << std::endl;
   
+  std::cout << "Value of acdVolId.isAcd() is " << acdVolId.isAcd() 
+            << std::endl;
+
   idents::VolumeIdentifier acdVolId2;
   acdVolId2.append(1); acdVolId2.append(0); acdVolId2.append(41);
   acdVolId2.append(0); acdVolId2.append(3);
@@ -65,6 +69,9 @@ int main()
   std::cout << "AcdId2 = " << acdId2.id() << std::endl;
   std::cout << acdId2.tile() << " " << acdId2.ribbon() << " " << acdId2.ribbonNum() 
       << " " << acdId2.ribbonOrientation() << std::endl;
+
+  std::cout << "Value of acdVolId2.isAcd() is " << acdVolId.isAcd() 
+            << std::endl;
 
   idents::VolumeIdentifier acdVolId3;
   acdVolId3.append(1); acdVolId3.append(1); acdVolId3.append(40);
@@ -76,15 +83,32 @@ int main()
   std::cout << acdId3.tile() << " " << acdId3.ribbon() << " " << acdId3.face() 
       << " " << acdId3.row() << " " << acdId3.column() << std::endl;
 
-  idents::VolumeIdentifier vId, vIdBad;
+  std::cout << "Value of acdVolId3.isAcd() is " << acdVolId.isAcd() 
+            << std::endl;
+
+  std::cout << "Value of acdVolId3.isCal() is " << acdVolId.isCal() 
+            << std::endl;
+
+  std::cout << "Value of acdVolId3.isTkr() is " << acdVolId.isTkr() 
+            << std::endl;
+  
+
+  idents::VolumeIdentifier vIdCal, vIdBad;
   unsigned yNum = 1, xNum = 2, layer = 5, column = 10;
-  vId.append(0);        // LATobject = towers
-  vId.append(yNum); 
-  vId.append(xNum);        // xnum
-  vId.append(0);        // towerObject = Calorimeter
-  vId.append(layer);        // layer
-  vId.append(0);        // orientation
-  vId.append(column);       // column
+  vIdCal.append(0);        // LATobject = towers
+  vIdCal.append(yNum); 
+  vIdCal.append(xNum);        // xnum
+  vIdCal.append(0);        // towerObject = Calorimeter
+  vIdCal.append(layer);        // layer
+  vIdCal.append(0);        // orientation
+  vIdCal.append(column);       // column
+
+  std::cout << "Value of vIdCal.isAcd() is " << vIdCal.isAcd() 
+            << std::endl;
+  std::cout << "Value of vIdCal.isCal() is " << vIdCal.isCal() 
+            << std::endl;
+  std::cout << "Value of vIdCal.isTkr() is " << vIdCal.isTkr() 
+            << std::endl;
 
   vIdBad.append(0);        // LATobject = towers
   vIdBad.append(yNum); 
@@ -94,28 +118,115 @@ int main()
   vIdBad.append(0);        // orientation
   vIdBad.append(column);       // column
 
+  std::cout << "Value of vIdBad.isAcd() is " << vIdBad.isAcd() 
+            << std::endl;
+  std::cout << "Value of vIdBad.isCal() is " << vIdBad.isCal() 
+            << std::endl;
+  std::cout << "Value of vIdBad.isTkr() is " << vIdBad.isTkr() 
+            << std::endl;
 
-  idents::CalXtalId xtalOk(vId);
-  if ((xtalOk.getTower() != (4*yNum  + xNum) ) ||
-      (xtalOk.getLayer() != layer)             ||
-      (xtalOk.getColumn() != column) ) {
-    std::string badConstructor("CalXtalId constructor from volId failed");
+
+  idents::CalXtalId xtalOk(vIdCal);
+  if ((xtalOk.getTower() != (int) (4*yNum  + xNum) ) ||
+      (xtalOk.getLayer() != (int) layer)             ||
+      (xtalOk.getColumn() != (int) column) ) {
+    std::string badConstructor("CalXtalId constructor from vIdCal failed");
     throw std::logic_error(badConstructor);
   }
   else {
-    std::cout << "Built CalXtalId properly from VolumeIdentifier" << std::endl;
+    std::cout << "Built CalXtalId properly from vIdCal" << std::endl;
   }
   
   try {
     idents::CalXtalId xtalBad(vIdBad);
-    std::string falsePositive("CalXtalId constructor failed to fail!");
+    std::string 
+      falsePositive("CalXtalId constructor for vIdBad failed to fail!");
     throw std::logic_error(falsePositive);
   }
   catch (std::invalid_argument ex) {
     std::cout << 
-      "Caught expected invalid_argument exception in CalXtalId constructor" 
-              << std::endl;
-    return 0;
+      "Caught expected invalid_argument exception in CalXtalId constructor " 
+              << "with input vIdBad" << std::endl;
   }
+
+  // Tests for TkrId class. Shouldn't be able to use valid acd id as input:
+  try {
+    idents::TkrId xtalBad(acdVolId);
+    std::string 
+      falsePositive("TkrId constructor for acdVolId failed to fail!");
+    throw std::logic_error(falsePositive);
+  }
+  catch (std::invalid_argument ex) {
+    std::cout << 
+      "Caught expected invalid_argument exception in TkrId constructor " 
+              << "with input acdVolId " << std::endl;
+  }
+
+  // Make a sensible tkr VolumeIdentifier
+  idents::VolumeIdentifier vIdTkr, vIdTkrTrunc;
+  unsigned tray = 3;
+  unsigned view = 1;
+  unsigned botTop = 0;  // bottom
+  unsigned ladder = 1;
+  unsigned wafer = 2;
+
+  vIdTkr.append(0); vIdTkrTrunc.append(0);        // LATobject = towers
+  vIdTkr.append(yNum); vIdTkrTrunc.append(yNum);
+  vIdTkr.append(xNum); vIdTkrTrunc.append(xNum);
+  vIdTkr.append(1);    vIdTkrTrunc.append(1);    // towerObject = Tracker
+  vIdTkr.append(tray); vIdTkrTrunc.append(tray);
+  vIdTkr.append(view); vIdTkrTrunc.append(view);
+
+
+  vIdTkr.append(botTop);
+  vIdTkr.append(ladder);
+  vIdTkr.append(wafer);
+
+  try {
+    idents::TkrId tId(vIdTkr);
+
+    std::cout << "Made TkrId with TowerY, TowerX = " 
+              << yNum << ", " << xNum << std::endl;
+    std::cout << " tray = " << tray << ", view = " << view << std::endl;
+    std::cout << " (botTop, ladder, wafer) = (" << botTop << ", ";
+    std::cout << ladder << ", " << wafer << ")" << std::endl;
+
+    std::cout << "Getting tower Y and x : "  
+              <<  tId.getTowerY() << ", " << tId.getTowerX() << std::endl;
+    std::cout << "Getting tray and view : "
+              << tId.getTray() << ", " << tId.getView() << std::endl;
+    std::cout << "Getting botTop, ladder, wafer : "
+              << tId.getBotTop() << ", " << tId.getLadder() 
+              <<   ", " << tId.getWafer() << std::endl;
+  }
+  catch (std::invalid_argument ex) {
+    std::cout << "Failed to construct TkrId from vIdTkr " << std::endl;
+  }
+
+  try {
+    idents::TkrId tIdTrunc(vIdTkrTrunc);
+    std::cout << "tIdTrunc is missing botTop,ladder,wafer " << std::endl;
+    std::cout << "vIdTkrTrunc size is " << vIdTkrTrunc.size() << std::endl;
+    unsigned val;
+    std::cout << "Fetching fields from corresponding TkrId.. " << std::endl;
+    val = tIdTrunc.getTowerY(); std::cout << val << " "; std::cout.flush();
+    val = tIdTrunc.getTowerX(); std::cout << val << " "; std::cout.flush();
+    val = tIdTrunc.getTray(); std::cout << val << " "; std::cout.flush();
+    val = tIdTrunc.getView(); std::cout << val << " "; std::cout.flush();
+    val = tIdTrunc.getBotTop(); std::cout << val << " "; std::cout.flush();
+    val = tIdTrunc.getLadder(); std::cout << val << " "; std::cout.flush();
+    val = tIdTrunc.getWafer(); std::cout << val << " " << std::endl; 
+    std::cout.flush();
+  }
+  catch (std::domain_error ex) {
+    std::string what(ex.what());
+    std::cout << std::endl;
+    std::cout << "Examining tIdTrunc fields, caught exception " 
+              << what << std::endl;
+  }
+  catch (std::invalid_argument ex) {
+    std::cout << "Failed to construct TkrId from vIdTrunc " << std::endl;
+  }
+
   return 0;
 }
